e586bace28167fba49190ce801b09f1cd471a845
JeremyAdam <83097265+JeremyAdam@users.noreply.github.com>
2022-02-09 10:05:51 -0700

    Add files via upload

--- C# - Nickname Generator/Program.cs
+++ C# - Nickname Generator/Program.cs
@@ -3,34 +3,29 @@ using System.Collections.Generic;
 
 namespace Test
 {
-    class nicknameVariables
-    {
-        public bool menuOpen = true;
-        public string userFirstName;
-        public string userLastName;
-        public int select;
-        public bool programOpenedBefore = false;
-        public string [] nicknamesArray = {"The Quick", "The Hacker", "The Big", "The Small", "The Weird"};
-        public bool run = true; // Keeps The Whole Thing Under A Loop
-
-    }
     class nicknameGenerator {
-        static void Main(string[] agrs)
+        static void Main(string[] args)
         {
-            nicknameVariables program = new nicknameVariables(); // Creates An Object
             Random random = new Random(); // Helps Create The Random Int
-            List<string> nicknamesList = new List<string>(program.nicknamesArray); // Creates List
-            while (program.run == true)
+            bool menuOpen = true; // To Load Menu
+            string userFirstName = ""; // Users Last Name
+            string userLastName = ""; // Users First Name
+            int select = 0; // For Selecting From menu
+            bool programOpenedBefore = false; // To Start Program By Asking For Name
+            string [] nicknamesArray = {"The Quick", "The Hacker", "The Big", "The Small", "The Weird"};
+            List<string> nicknamesList = new List<string>(nicknamesArray); // Creates List
+            bool run = true; // Keeps The Whole Thing Under A Loop
+            while (run)
             {
-                if (program.programOpenedBefore == false)
+                if (programOpenedBefore == false)
                 { // Checks For First Time Opening Program
                     Console.WriteLine("Please enter your first name: ");
-                    program.userFirstName = Convert.ToString(Console.ReadLine());
+                    userFirstName = Convert.ToString(Console.ReadLine());
                     Console.WriteLine("Please enter your last name: ");
-                    program.userLastName = Convert.ToString(Console.ReadLine());
-                    program.programOpenedBefore = true;
+                    userLastName = Convert.ToString(Console.ReadLine());
+                    programOpenedBefore = true;
                 }
-                while (program.menuOpen == true)
+                while (menuOpen == true)
                 { // Creates The Main Menu
                     Console.WriteLine(" ");                
                     Console.WriteLine("---NICKNAME GENERATOR");
@@ -41,39 +36,40 @@ namespace Test
                     Console.WriteLine("5: Remove a nickname.");
                     Console.WriteLine("6: Exit.");
                     Console.WriteLine("---SELECT (1-6): ");
-                    program.select = Convert.ToInt32(Console.ReadLine());
-                    program.menuOpen = false;
+                    select = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(" ");  
+                    menuOpen = false;
                 }
-                if (program.select == 1)
+                if (select == 1)
                 { // Change Name Variables
                     Console.WriteLine("Please enter your new first name: ");
-                    program.userFirstName = Convert.ToString(Console.ReadLine());
+                    userFirstName = Convert.ToString(Console.ReadLine());
                     Console.WriteLine("Please enter your new last name: ");
-                    program.userLastName = Convert.ToString(Console.ReadLine());
-                    program.select = 0;
-                    program.menuOpen = true;
-                } else if (program.select == 2)
+                    userLastName = Convert.ToString(Console.ReadLine());
+                    select = 0;
+                    menuOpen = true;
+                } else if (select == 2)
                 { // Generate One Nickname
                     int randNum = random.Next(0, nicknamesList.Count);
-                    Console.WriteLine(program.userFirstName + " " + nicknamesList[randNum] + " " + program.userLastName);
-                    program.select = 0;
-                    program.menuOpen = true;
-                } else if (program.select == 3)
+                    Console.WriteLine(userFirstName + " " + nicknamesList[randNum] + " " + userLastName);
+                    select = 0;
+                    menuOpen = true;
+                } else if (select == 3)
                 { // Generate All Nicknames In List
                     for (int i = 0; i < nicknamesList.Count; i++)
                     {
-                        Console.WriteLine(program.userFirstName + " " + nicknamesList[i] + " " + program.userLastName);
+                        Console.WriteLine(userFirstName + " " + nicknamesList[i] + " " + userLastName);
                     }
-                    program.select = 0;
-                    program.menuOpen = true;
-                } else if (program.select == 4)
+                    select = 0;
+                    menuOpen = true;
+                } else if (select == 4)
                 { // Add A Nickname To The List
                     Console.WriteLine("Please add your new nickname: ");
                     string addToList = Convert.ToString(Console.ReadLine());
                     nicknamesList.Add(addToList);
-                    program.select = 0;
-                    program.menuOpen = true;
-                } else if (program.select == 5)
+                    select = 0;
+                    menuOpen = true;
+                } else if (select == 5)
                 { // Remove A Nickname From The List
                     foreach (string i in nicknamesList)
                     {
@@ -90,11 +86,16 @@ namespace Test
                     {
                         Console.WriteLine("No nickname with that name was found");
                     }
-                    program.select = 0;
-                    program.menuOpen = true;
-                } else if (program.select == 6)
+                    select = 0;
+                    menuOpen = true;
+                } else if (select == 6)
                 { // End Program
-                    program.run = false;
+                    run = false;
+                } else
+                {
+                    Console.WriteLine("Value does not work");
+                    select = 0;
+                    menuOpen = true;
                 }
             } 
         }
